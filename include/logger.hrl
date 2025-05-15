0c61db2c468055b2783fdb10f11ab341e64bf6cf
Pankaj Soni <pankajsoni19@live.com>
2025-05-15 23:26:08 +0530

    fix lager

--- include/logger.hrl
+++ include/logger.hrl
@@ -1,13 +1,21 @@
-%% logging callbacks
+-ifndef(PRINT).
 
--define(PRINT(Format, Args), io:format(Format, Args)).
+-define(PRINT(Format, Args), 
+    _ = io:format(Format, Args)).
 
--define(DEBUG(Format, Args), lager:log(debug, self(), Format, Args)).
+-define(DEBUG(Format, Args), 
+    _ = lager:debug(Format, Args)).
 
--define(INFO_MSG(Format, Args), lager:log(info, self(), Format, Args)).
+-define(INFO_MSG(Format, Args), 
+    _ = lager:info(Format, Args)).
 
--define(WARNING_MSG(Format, Args), lager:log(warning, self(), Format, Args)).
+-define(WARNING_MSG(Format, Args), 
+    _ = lager:warning(Format, Args)).
 
--define(ERROR_MSG(Format, Args), lager:log(error, self(), Format, Args)).
+-define(ERROR_MSG(Format, Args), 
+    _ = lager:error(Format, Args)).
 
--define(CRITICAL_MSG(Format, Args), lager:log(critical, self(), Format, Args)).
+-define(CRITICAL_MSG(Format, Args), 
+    _ = lager:critical(Format, Args)).
+
+-endif.
